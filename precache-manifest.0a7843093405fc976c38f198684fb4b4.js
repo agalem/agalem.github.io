935493395f783072eedab990c1aacbe6bbb1e9e2
Agnieszka Lempaszek <agnieszka.lempaszek@student.uj.edu.pl>
2020-04-14 15:32:20 +0200

    Updates

---
+++ precache-manifest.0a7843093405fc976c38f198684fb4b4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544901cd4a87c4fe48f4cb648fd0e68e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29268cf61695ea5eb346",
+    "url": "/static/css/main.304e5117.chunk.css"
+  },
+  {
+    "revision": "e50caa08fa8e290e1e51",
+    "url": "/static/js/2.1da4eaa8.chunk.js"
+  },
+  {
+    "revision": "36c3ab509122089b5afed57cdf717dcc",
+    "url": "/static/js/2.1da4eaa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29268cf61695ea5eb346",
+    "url": "/static/js/main.fe55884b.chunk.js"
+  },
+  {
+    "revision": "a27870e5f03a3425f6aa",
+    "url": "/static/js/runtime-main.c70048c8.js"
+  },
+  {
+    "revision": "cc1b971a30f5e3e321789dbb65de3767",
+    "url": "/static/media/bikeTemplate.cc1b971a.png"
+  },
+  {
+    "revision": "98046eef05323164fc1ee6a2b6ae10ec",
+    "url": "/static/media/card_form.98046eef.png"
+  },
+  {
+    "revision": "80eb41e8ffd65fe932f42a4c6614cf0a",
+    "url": "/static/media/developer.80eb41e8.svg"
+  },
+  {
+    "revision": "0629cb31afbcea69717666ab349b5cb6",
+    "url": "/static/media/mail.0629cb31.jpg"
+  },
+  {
+    "revision": "f480297af3c6f8999ab4b8af950f2d90",
+    "url": "/static/media/portfolio.f480297a.png"
+  },
+  {
+    "revision": "d0bd32165a33cc9dbb15a405c86157e0",
+    "url": "/static/media/project17.d0bd3216.png"
+  },
+  {
+    "revision": "131e12976cbe03e7be28733b2082280e",
+    "url": "/static/media/project3.131e1297.png"
+  },
+  {
+    "revision": "7071d78a6340958231a6135b7c9e89bd",
+    "url": "/static/media/project37.7071d78a.png"
+  },
+  {
+    "revision": "cb06258408da84870520c4d3d6d8f3c2",
+    "url": "/static/media/sudoku.cb062584.png"
+  }
+]);
