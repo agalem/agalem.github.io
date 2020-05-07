abfaef20ef4f333fe96e4b4e40b2645474ca9133
Agnieszka Lempaszek <aga.lempaszek94@gmail.com>
2020-05-07 23:51:44 +0200

    portfolio build

---
+++ precache-manifest.2bb531793a9115bdb78b952a8d0a4792.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b7288d2e7f2660cd63da800a3d8138",
+    "url": "./index.html"
+  },
+  {
+    "revision": "73e05fd88b8bf153adb2",
+    "url": "./static/css/main.476d9223.chunk.css"
+  },
+  {
+    "revision": "41f9dc24f2e36b8df0f2",
+    "url": "./static/js/2.c8996688.chunk.js"
+  },
+  {
+    "revision": "d0d6b5033fd77eb6038046252d67d3b7",
+    "url": "./static/js/2.c8996688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e05fd88b8bf153adb2",
+    "url": "./static/js/main.434fcffe.chunk.js"
+  },
+  {
+    "revision": "705be1eaeebc943a31b4",
+    "url": "./static/js/runtime-main.e31e8089.js"
+  },
+  {
+    "revision": "cc1b971a30f5e3e321789dbb65de3767",
+    "url": "./static/media/bikeTemplate.cc1b971a.png"
+  },
+  {
+    "revision": "98046eef05323164fc1ee6a2b6ae10ec",
+    "url": "./static/media/card_form.98046eef.png"
+  },
+  {
+    "revision": "80eb41e8ffd65fe932f42a4c6614cf0a",
+    "url": "./static/media/developer.80eb41e8.svg"
+  },
+  {
+    "revision": "0629cb31afbcea69717666ab349b5cb6",
+    "url": "./static/media/mail.0629cb31.jpg"
+  },
+  {
+    "revision": "f480297af3c6f8999ab4b8af950f2d90",
+    "url": "./static/media/portfolio.f480297a.png"
+  },
+  {
+    "revision": "d0bd32165a33cc9dbb15a405c86157e0",
+    "url": "./static/media/project17.d0bd3216.png"
+  },
+  {
+    "revision": "131e12976cbe03e7be28733b2082280e",
+    "url": "./static/media/project3.131e1297.png"
+  },
+  {
+    "revision": "7071d78a6340958231a6135b7c9e89bd",
+    "url": "./static/media/project37.7071d78a.png"
+  },
+  {
+    "revision": "cb06258408da84870520c4d3d6d8f3c2",
+    "url": "./static/media/sudoku.cb062584.png"
+  }
+]);
